0af1139ec9671a1484418389b4a14b3117988d6a
CindyJaneP <Cindy-Jane.Peregrino@sanlam.co.za>
2023-05-10 15:51:49 +0200

    Added test fixtures

--- NUnitIntro2/Program.cs
+++ NUnitIntro2/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using NUnit.Framework;
+
 Console.WriteLine("Hello, Cindy! Welcome to Paradise!");
 
+namespace Tests
+{
+    [TestFixture]
+        public class TestFixture
+    {
+        [Test]
+        public void DisplayWelcome()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
